6bc4e2f60a386abd0dc856fe2a5d024a1800afac
Snehal Sutar <svs.sutar.1@gmail.com>
2016-04-15 13:37:37 -0500

    Added Login screen..

---
+++ app/src/main/java/com/svs/myprojects/bollywoodquiz/LoginActivity.java
@@ -0,0 +1,25 @@
+package com.svs.myprojects.bollywoodquiz;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+    }
+
+}
